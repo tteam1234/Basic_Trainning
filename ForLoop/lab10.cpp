66cd47e854cc1db54e05ecfbe4dedffc2b394430
tteam1234 <phithaitam@gmail.com>
2022-01-01 02:08:52 +0700

    add lab10

---
+++ ForLoop/lab10.cpp
@@ -0,0 +1,17 @@
+// tinh tong tu 1 den n
+// thu vien
+#include<iostream>
+#include<cmath>
+using namespace std;
+// ham main
+int main(){
+	// khai bao bien
+	int n;
+    int x;
+	// xu li
+    cin>>x; // nhap x
+	cin>>n; // nhap n
+	// tinh tong
+	cout<<pow(x,n);// in tong
+	return 0;
+}
