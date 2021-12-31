4d926c06d90aa079877f0c6dc90079ab0c9be9ea
tteam1234 <phithaitam@gmail.com>
2022-01-01 02:06:31 +0700

    thêm lab9

---
+++ ForLoop/lab9.cpp
@@ -0,0 +1,18 @@
+// tinh tong tu 1 den n
+// thu vien
+#include<iostream>
+using namespace std;
+// ham main
+int main(){
+	// khai bao bien
+	int n;
+	float T = 1;
+	// xu li
+	cin>>n; // nhap
+	// tinh tong
+	for(int i = 2; i<=n; i++){
+		T*=i;// ép kiểu
+	}
+	cout<<s;// in tong
+	return 0;
+}
