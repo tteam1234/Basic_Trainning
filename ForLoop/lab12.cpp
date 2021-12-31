3dc9315cef4f10db0edc8551b5a0e3a73a2cacb4
tteam1234 <phithaitam@gmail.com>
2022-01-01 02:14:38 +0700

    add lab13

---
+++ ForLoop/lab12.cpp
@@ -0,0 +1,20 @@
+// tinh tong tu 1 den n
+// thu vien
+#include<iostream>
+#include<cmath>
+using namespace std;
+// ham main
+int main(){
+	// khai bao bien
+	int x,n;
+	float s = 0;
+	// xu li
+    cin>>x;
+	cin>>n; // nhap
+	// tinh tong
+	for(int i = 1; i<=n; i++){
+		s+=(float)pow(x,i);// ép kiểu
+	}
+	cout<<s;// in tong
+	return 0;
+}
