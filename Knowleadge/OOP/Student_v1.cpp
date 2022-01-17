73d2649d0d98ea0b31a333596473d01316877d47
tteam1234 <phithaitam@gmail.com>
2022-01-18 02:35:57 +0700

    Them thuoc tinh cua class Student

---
+++ Knowleadge/OOP/Student_v1.cpp
@@ -0,0 +1,13 @@
+class Student{
+    private:
+        string id;
+        string name;
+        float math;
+        float physical;
+        float chemistry;
+    public:
+    // method
+}
+int main(){
+    
+}
