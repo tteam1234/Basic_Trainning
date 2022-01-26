5e3337652fb532323fedf09298219e4ffaec4875
nvtho999 <nguyenvantho31072003@gmail.com>
2022-01-26 21:29:58 +0700

    menu

--- Exam/Struct/PhanSo.cpp
+++ Exam/Struct/PhanSo.cpp
@@ -6,18 +6,51 @@ typedef struct PhanSo{
 };
 void nhap(PhanSo &phanso);
 void xuat(PhanSo phanso);
+void menu(PhanSo phanso);// dinh nghia ham menu
 int main(){
     PhanSo phanso;
     nhap(phanso);
-    xuat(phanso);
+    menu(phanso);
     return 0;
 }
 void nhap(PhanSo &phanso){
-    cout<<"Nhap phan tusonguyen: ";
+    cout<<"Nhap tu so: ";
     cin>>phanso.tusonguyen;
-    cout<<"Nhap phan mausonguyen: ";
+    cout<<"Nhap mau so: ";
     cin>>phanso.mausonguyen;
 }
 void xuat(PhanSo phanso){
     cout<<phanso.tusonguyen<<"/"<<phanso.mausonguyen;
+}
+void menu(PhanSo phanso){
+    int luachon;
+    do{
+     cout<<"------------Menu------------"<<endl;
+    cout<<"1. nhap lai"<<endl;
+    cout<<"2. xuat ra phan so"<<endl;
+    cout<<"3. rut gon phan so"<<endl;
+    cout<<"nhap lua chon:";
+    cin>>luachon;
+    switch(luachon){
+        case 1: {
+            nhap(phanso);
+            break;
+        }
+        case 2: {
+            xuat(phanso);
+            break;
+        }
+        case 3: {
+            cout<<"rut gon phan so";
+            break;
+        }
+        default:{
+            cout<<"lua chon khong hop le";
+            break;
+        }
+
+    }
+        cout<<"Nhap phim 1 de thoat";
+        cin>>luachon;
+    }while(luachon!=1);       
 }
