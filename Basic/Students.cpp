b89fff13119432445718226ca326d9a91d93510b
tteam1234 <phithaitam@gmail.com>
2022-01-27 16:28:54 +0700

    menu

--- Basic/Students.cpp
+++ Basic/Students.cpp
@@ -15,6 +15,8 @@ void xuat(Student student);
 void xuat(Student students[], int numbers);
 bool tonTaiSinhVien(Student students[], int numbers, int id);
 Student timKiemSinhVienTheoMa(Student students[], int &numbers, int id);//id = 3
+Student timSinhVienCoGPACaoNhat(Student students[], int &numbers);
+Student timSinhVienCoGPANhoNhat(Student students[], int &numbers);
 int main(){
     Student students[MAX];// khai bao mang students
     int numbers;
@@ -81,6 +83,8 @@ void menu(Student students[], int &numbers){
         cout<<"1. Nhap lai"<<endl;
         cout<<"2. xuat danh sach"<<endl;
         cout<<"3. tim kiem sinh vien theo ma"<<endl;
+        cout<<"4. tim kiem sinh vien co diem cao nhat"<<endl;
+        cout<<"5. tim kiem sinh vien co diem thap nhat"<<endl;
         // chon chuc nang
         cout<<"Chon chuc nang: ";
         cin>>luachon;
@@ -100,12 +104,44 @@ void menu(Student students[], int &numbers){
                 if(tonTaiSinhVien(students,numbers,idcantim)){
                     xuat(timKiemSinhVienTheoMa(students,numbers,idcantim));
                 } else {
-                    cout<<"Khong co sinh vien can tim!";
+                    cout<<"Khong co sinh vien can tim!"<<endl;
                 }
+            }
+            case 4: {
+                cout<<"Sinh vien co diem cao nhat: "<<endl;
+                xuat(timSinhVienCoGPACaoNhat(students,numbers));
+                break;
+            }
+            case 5: {
+                cout<<"Sinh vien co diem thap nhat: "<<endl;
+                xuat(timSinhVienCoGPANhoNhat(students,numbers));
+                break;
+            }
+            default: {
+                cout<<"Lua chon khong hop le!!!";
+                break;
             }
         }
         // nhap lua chon de thoat
         cout<<"Nhan phim 1 de thoat!";
         cin>>luachon;
     }while(luachon!=1);
+}
+Student timSinhVienCoGPACaoNhat(Student students[], int &numbers){
+    Student max = students[0];
+    for(int i=1;i<numbers;i++){
+        if(students[i].gpa>max.gpa){
+            max = students[i];
+        }
+    }
+    return max;
+}
+Student timSinhVienCoGPANhoNhat(Student students[], int &numbers){
+    Student min = students[0];// moc nho nhat la dau tien
+    for(int i=1;i<numbers;i++){
+        if(students[i].gpa<min.gpa){
+            min = students[i];
+        }
+    }
+    return min;
 }
