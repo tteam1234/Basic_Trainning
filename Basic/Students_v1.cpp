82bba7c92d209b404d06f46f84f35dc0d4f151ba
tteam1234 <phithaitam@gmail.com>
2022-01-27 18:21:24 +0700

    them menu va them fflush(stdin)

--- Basic/Students_v1.cpp
+++ Basic/Students_v1.cpp
@@ -11,15 +11,18 @@ typedef struct Student{
 };
 void nhap(Student &student);
 void nhap(Student students[], int &numbers);
+void menu(Student students[], int &numbers);
 int main(){
     Student students[MAX];
     int numbers;
     nhap(students, numbers);
+    menu(students, numbers);
     return 0;
 }
 void nhap(Student &student){
     cout<<"id: ";
     cin>>student.id;
+    fflush(stdin);
     cout<<"ho ten: ";
     getline(cin,student.name);
     cout<<"gioi tinh: ";
@@ -36,4 +39,32 @@ void nhap(Student students[], int &numbers){
         // nhap tung sinh vien
         nhap(students[i]);
     }
+}
+
+void menu(Student students[], int &numbers){
+    int luachon;
+    do{
+        // in ra man hinh menu
+        cout<<"------Menu--------"<<endl;
+        cout<<"1.nhap lai"<<endl;
+        // nhap lua chon
+        cout<<"Chon chuc nang: ";
+        cin>>luachon;
+        // xet chuc nang: 
+        switch(luachon){
+            case 1:
+            {
+                nhap(students, numbers);
+                break;
+            }
+            default:
+            {
+                cout<<"Lua chon khong hop le!"<<endl;
+                break;
+            }
+        }
+        // xem nguoi dung co muon tiep tuc khong?
+        cout<<"Nhan 1 de thoat:";
+        cin>>luachon;
+    }while(luachon!=1);    
 }
